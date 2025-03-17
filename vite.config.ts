8a1a90b968d45f72545333fa9ce4b29562dd1caf
lsh908400 <kio11010@naver.com>
2025-03-17 22:42:52 +0900

    error - 배포 cors설정 에러

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        '/uploads': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
       }
-    }
-  },
+    },
+  }
 })
